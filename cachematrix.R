b28485db452b51a22ede1c9e0c9dccd2dae1e4db
JesperKJ <jesper.alt@gmail.com>
2017-05-31 23:30:45 +0200

    Assignment solution

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,39 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Following two functions used to calculate the inverse of a matrix
+## In order to speed up computation time the inverse is saved in a global
+## variable that can be accesed in environments other than it's own.
+## Two functions are defined makeChacheMatrix and cacheSolve.  and the second function calls the cashed 
+## value of the inverted matrix if it exists or calculates the inverse. 
 
-## Write a short comment describing this function
+## The first function is a list of functions setting and getting 
+## a matrix and the inverse of a matrix (x)
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(solve) m <<- solve
+  getinverse <- function() m
+  list(set = set, 
+       get = get, 
+       setinverse = setinverse, 
+       getinverse = getinverse)
 }
 
-
-## Write a short comment describing this function
+## The function cacheSolve checks if there is a saved value of the inverted matrix
+## and uses the stored value if it is present. Otherwise it calculates the inverse
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  m <- x$getinverse()
+  if (!is.null(m)){
+    message("getting stored inverse")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setinverse(m)
+  m
 }
